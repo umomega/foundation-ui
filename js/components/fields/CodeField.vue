5694c6f68fd9eb81039bb755d74657d2e1275d99
Egemen Berker Kızılcan <egemenkizilcan@gmail.com>
2021-02-25 12:47:42 +0300

    Updated dependencies and made code field font size smaller

--- js/components/fields/CodeField.vue
+++ js/components/fields/CodeField.vue
@@ -1,44 +1,54 @@
 <template>
-	<div>
-		<label v-if="label != undefined" class="label is-uppercase">{{ label }}</label>
-		<div class="control">
-			<div class="code-editor">
-				<prism-editor v-model="code" :highlight="highlighter" :line-numbers="true" :readonly="readonly" @input="updateValue"></prism-editor>
-			</div>
-		</div>	
-		<p class="help is-danger" v-if="anyErrors()" v-text="getErrorMessage()"></p>
-		<p class="help" v-else v-html="hint"></p>
-	</div>
+  <div>
+    <label v-if="label != undefined" class="label is-uppercase">{{
+      label
+    }}</label>
+    <div class="control">
+      <div class="code-editor is-size-9">
+        <prism-editor
+          v-model="code"
+          :highlight="highlighter"
+          :line-numbers="true"
+          :readonly="readonly"
+          @input="updateValue"
+        ></prism-editor>
+      </div>
+    </div>
+    <p class="help is-danger" v-if="anyErrors()" v-text="getErrorMessage()"></p>
+    <p class="help" v-else v-html="hint"></p>
+  </div>
 </template>
 
 <script>
-import Field from '../../mixins/Field'
-import {PrismEditor} from 'vue-prism-editor'
-import 'vue-prism-editor/dist/prismeditor.min.css'
-import {highlight, languages} from 'prismjs/components/prism-core'
-import 'prismjs/themes/prism-tomorrow.css'
+import Field from "../../mixins/Field";
+import { PrismEditor } from "vue-prism-editor";
+import "vue-prism-editor/dist/prismeditor.min.css";
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/themes/prism-tomorrow.css";
 
 export default {
-	mixins: [ Field ],
-	components: { PrismEditor },
-	data() { return {
-		code: this.compileValue()
-	}},
-	watch: {
-		value(to) {
-			this.code = this.compileValue()
-		}
-	},
-	methods: {
-		highlighter(code) {
-    		return highlight(code, languages.clike);
-		},
-		updateValue(code) {
-			this.$emit('input', code)
-		},
-		compileValue() {
-			return this.value || ''
-		}
-	}
-}
+  mixins: [Field],
+  components: { PrismEditor },
+  data() {
+    return {
+      code: this.compileValue(),
+    };
+  },
+  watch: {
+    value(to) {
+      this.code = this.compileValue();
+    },
+  },
+  methods: {
+    highlighter(code) {
+      return highlight(code, languages.clike);
+    },
+    updateValue(code) {
+      this.$emit("input", code);
+    },
+    compileValue() {
+      return this.value || "";
+    },
+  },
+};
 </script>
